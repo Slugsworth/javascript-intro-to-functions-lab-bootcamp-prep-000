40d7531edf2e78a9448146dff2ca8d836a30d61d
Rick Battipede <rbattipede@gmail.com>
2019-05-01 02:58:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ function sayHiToGrandma(string){
   var lowercase = "hello";
   var grandma = "I love you, Grandma";
   if(string===lowercase){
-    return "I can\'t hear you"
-    
-  }
+    return "I can\'t hear you!"
+    } else if(string===uppercase){
+      return
+    }
   
   if
 }
