eb3d895d71e1f3a1b68f9155a66ea5c249249d8a
Rick Battipede <rbattipede@gmail.com>
2019-05-01 02:47:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string) {
   console.log(string.toUpperCase());
 }
 
-function whisper(string) {
-  return string.toLowerCase();
+function logWhisper(string) {
+  console.log(string.toLowerCase());
 }
 
